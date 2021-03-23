f0f611795b56487357f00152051085f8218609e1
Darren Reid <darren@reidmail.org>
2021-03-24 10:02:56 +1100

    Rename DTO, add optional property to highlight common query type.

--- CovidVaccinationWatch.ServiceModel/VaccinationRequests.cs
+++ CovidVaccinationWatch.ServiceModel/VaccinationRequests.cs
@@ -4,9 +4,9 @@ using ServiceStack;
 namespace CovidVaccinationWatch.ServiceModel
 {
     [Route("/vaccination_rate")]
-    public class QueryVaccinationRatesByLocation : QueryDb<VaccinationData>
+    public class QueryVaccinationRates : QueryDb<VaccinationData>
     {
-        
+        public string Location { get; set; }
     }
 
     [Route("/vaccination_rate/reset")]
